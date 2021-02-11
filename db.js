5c23e1b081e057814f8367ab601a2843abd6f23f
websitebuildr <78500863+websitebuildr@users.noreply.github.com>
2021-02-11 15:38:14 +0530

    Delete db.js

--- db.js
+++
@@ -1,7 +0,0 @@
-firebase.initializeApp({
-  apiKey: 'AIzaSyCAWVF2xvzCBWQH3WHKNj2-XoD1Joa0IyA',
-  authDomain: 'havenlands-feedback-data.firebaseapp.com',
-  projectId: 'havenlands-feedback-data'
-});
-
-const db = firebase.firestore();  
