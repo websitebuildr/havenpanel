37a603462ec5528d9c0be6fc102444d7b9847f75
websitebuildr <78500863+websitebuildr@users.noreply.github.com>
2021-02-11 15:37:59 +0530

    Delete checklogin.js

--- checklogin.js
+++
@@ -1,9 +0,0 @@
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        document.getElementById('page').style.display = "block"
-    } else {
-      // No user is signed in.
-      document.getElementById('page').style.display = "none"
-      location.replace('authPage.html')
-    }
-  });
