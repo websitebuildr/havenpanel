e0073b7ec5c0eed4a059173a85c47eea60063b01
websitebuildr <78500863+websitebuildr@users.noreply.github.com>
2021-02-15 09:26:26 +0530

    added bio upload function

--- profile.js
+++ profile.js
@@ -1,12 +1,25 @@
 function updateProfile() {
     var user = firebase.auth().currentUser
+    var email = user.email
     var displayname = document.getElementById('displayname').value
+    var bio = document.getElementById('bio').value
+    // updateProfileonDb(email)
     user.updateProfile({
-        displayName : displayname
+        displayName : displayname,
     })
     .then(()=>{
-        alert('Account updated successfully!')
-        location.replace('dashboard.html')
+        db.collection('users').doc(email).update({
+            name:displayname,
+            bio:bio
+        })
+        .then(()=>{
+            alert('Account updated successfully!')
+            location.replace('dashboard.html')
+        })
+        .catch((error)=>{
+            alert("We couldn't update your profile.....\nErrord : "+ error.message)
+        })
+        
     })
     .catch((error)=>{
         alert("We couldn't update your profile.....\nErrord : "+ error.message)
